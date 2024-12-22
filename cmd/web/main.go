d51d2311b52308378b71ff13686d460f53b0ffc7
Pranava116 <pranavagrao116@gmail.com>
2024-12-22 21:31:46 +0530

    Better loggers and servre struct

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	mux := http.NewServeMux()
+	addr := flag.String("addr", ":4000", "HTTP Network Port")
+	flag.Parse()
+	infoLogger := log.New(os.Stdout, "INFO\t", log.Ltime|log.Ldate)
+	erroLogger := log.New(os.Stderr, "ERROR\t", log.Ltime|log.Ldate|log.Lshortfile)
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: erroLogger,
+		Handler:  mux,
+	}
 	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippetbox/view", snippetView)
 	mux.HandleFunc("/snippetbox/create", snippetCreate)
-	log.Print("Server started on port 4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	infoLogger.Printf("Server started on port %s", *addr)
+	err := srv.ListenAndServe()
+	erroLogger.Fatal(err)
 }
